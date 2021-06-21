145b3a1d07c342044febe59fbd6f42ae9b898b77
Camila Di Toro <camiladitoro@gmail.com>
2021-06-21 00:16:07 -0300

    coments added

--- tpe.sh
+++ tpe.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Hay que aclarar en la documentacion la forma correcta de llamar al programa
-# Nosotros vamos a usar que primero se pasa la categoria y luego el año.
-# Por como definí el egrep en la función check_category, el programa NO es case sensitive.
-
 . ./full_validations.sh
 
 check_parameters $# $*
@@ -11,10 +7,16 @@ valid_parameters=$?
 
 if [ $valid_parameters -eq 0 ]
     then
+
       # Pasamos a minuscula lo que tenemos en el parametro $2
+
       category=${2,,}
+      
       curl -X GET "http://api.sportradar.us/nascar-ot3/${category}/${1}/drivers/list.xml?api_key=${SPORTRADAR_API}" -o drivers_list.xml
       curl -X GET "http://api.sportradar.us/nascar-ot3/${category}/${1}/standings/drivers.xml?api_key=${SPORTRADAR_API}" -o drivers_standings.xml
+      
+      #Borramos el namespace
+
       sed -i 's/ xmlns="[^"]*"//' drivers_list.xml && sed -i 's/ xmlns="[^"]*"//' drivers_standings.xml
 fi
 
